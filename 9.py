344b06b9189213371260fb8d937cce7662db4d28
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2024-07-26 11:29:51 -0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,34 +1,31 @@
-cpf = input("Digite o CPF (apenas números): ")
+cpf = input("Digite o CPF: ")
+primeiro_digito = 0
+segundo_digito = 0
+soma = 0
+soma2 = 0
 
-# Remover possíveis caracteres não numéricos
-cpf = ''.join([char for char in cpf if char.isdigit()])
+# Validação do primeiro dígito
+for cont in range(10, 1, -1):
+    numero = cpf[10 - cont]
+    soma += int(numero) * cont
 
-# Verificar se o CPF tem 11 dígitos
-if len(cpf) != 11:
-    print("CPF inválido")
+if soma % 11 < 2:
+    primeiro_digito = 0
 else:
-    # Calcular o primeiro dígito verificador
-    soma = 0
-    for i in range(9):
-        soma += int(cpf[i]) * (10 - i)
-    
-    if soma % 11 < 2:
-        primeiro_digito = 0
-    else:
-        primeiro_digito = 11 - (soma % 11)
-    
-    # Calcular o segundo dígito verificador
-    soma2 = 0
-    for i in range(10):
-        soma2 += int(cpf[i]) * (11 - i)
-    
-    if soma2 % 11 < 2:
-        segundo_digito = 0
-    else:
-        segundo_digito = 11 - (soma2 % 11)
-    
-    # Verificar se os dígitos calculados conferem com os fornecidos
-    if cpf[-2] == str(primeiro_digito) and cpf[-1] == str(segundo_digito):
-        print("CPF válido")
-    else:
-        print("CPF inválido")
+    primeiro_digito = 11 - (soma % 11)
+
+# Validação do segundo dígito
+for cont in range(11, 1, -1):
+    numero = cpf[11 - cont]
+    soma2 += int(numero) * cont
+
+if soma2 % 11 < 2:
+    segundo_digito = 0
+else:
+    segundo_digito = 11 - (soma2 % 11)
+
+# Verificação final
+if cpf[-2] == str(primeiro_digito) and cpf[-1] == str(segundo_digito):
+    print("CPF válido")
+else:
+    print("CPF inválido")
